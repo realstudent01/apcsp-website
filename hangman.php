05b83febb92bac170c1d110f323e6778971c8d16
Joseph Harrington <josephdanielsd@gmail.com>
2018-06-12 15:22:55 +0000

    website

---
+++ hangman.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hangman: The Game</title>
+  </head>
+
+
+  <body>
+
+    <h1>Hangman: The Game</h1>
+
+    <?php
+
+	$input = "";
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	  $input = test_input
+	}
+
+    ?>
+
+  </body>
+</html>
---
+++ hangmanstart.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hangman Home Page</title>
+  </head>
+
+
+  <body>
+
+	<h1>Hangman Home Page</h1>
+
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
